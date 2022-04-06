80dcbe928aba2d276d622022002be3f815f5b78a
Arthur Vianna <arthurpedrosa96.ap@gmail.com>
2022-04-06 15:28:13 -0300

    exercise of primitive types and operators

---
+++ fundamentos/bloco-04/exercicio-tipos-operacoes/index.js
@@ -0,0 +1,37 @@
+// Primitives Types
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof isEnrolled);
+
+console.log(typeof patientAge);
+
+patientId = '50';
+console.log(typeof patientId);
+
+// Operators 
+
+// 1. Crie uma costante chamada base e uma variável chamada height e atribua os seus respectivos valores: 5 e 8.
+
+const base = 5;
+const height = 8;
+
+// 2. Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela altura. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+
+const area = base * height;
+console.log(area);
+
+// 3. Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
+
+const perimeter = 2 * (5 + 8);
+console.log(perimeter);
+
