2b0712c507dbad3c7791388733d9b50cf7b0e0d9
Arthur Vianna <arthurpedrosa96.ap@gmail.com>
2022-04-08 09:07:54 -0300

    bonus exercise 2

--- fundamentos/bloco-04/dia-03/bonus1.js
+++ fundamentos/bloco-04/dia-03/bonus1.js
@@ -1,10 +1,10 @@
-// 1. Agora vamos trabalhar com algumas formas geométricas! Faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+// 1. Agora vamos trabalhar com algumas formas geométricas! Faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n .
 
-const n = 10;
+const n = 5;
 let finalDrawing = '';
 for (let column = 0; column < n; column += 1){
     for (let row = 0; row < n; row += 1){
-        finalDrawing += '*';
+        finalDrawing += '* ';
     }
     finalDrawing += '\n';
 }
