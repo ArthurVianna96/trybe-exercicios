7b18378d4d3cdb7fb7ef7283ae8763ebf4e80239
Arthur Vianna <arthurpedrosa96.ap@gmail.com>
2022-04-11 09:59:00 -0300

    exercise with for in and objects

---
+++ fundamentos/bloco-04/dia-04/exercicio-for-in-for-of/index.js
@@ -0,0 +1,25 @@
+// 1 - Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+};
+  
+for (let person in names) {
+    console.log(`Olá ${names[person]}`);
+}
+
+// 2 - Usando o objeto abaixo, utilize for/in e imprima um console.log com as chaves e valores desse objeto.
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+};
+
+console.log('-------------');
+  
+for (let key in car) {
+    console.log(key + ' -> ' + car[key]);
+}
