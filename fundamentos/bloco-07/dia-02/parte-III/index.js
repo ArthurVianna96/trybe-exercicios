2685b14ceadbde6a791526875b173c61368f8be6
Arthur Vianna <arthurpedrosa96.ap@gmail.com>
2022-05-09 18:02:43 -0300

    bonus fixation exercises

--- fundamentos/bloco-07/dia-02/parte-III/index.js
+++ fundamentos/bloco-07/dia-02/parte-III/index.js
@@ -68,8 +68,43 @@ console.log(getObjectSize(lesson1));
 
 const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
 
-
 console.log(allLessons);
 console.log(getTotalStudents(allLessons));
 console.log(getValueByNumber(lesson1, 0));
 console.log(verifyPair(lesson3, 'turno', 'noite'));
+
+// Bônus
+function countNumberOfStudentsInClass(lessonsObject, subject) {
+  let totalStudents = 0;
+  Object.keys(lessonsObject).forEach(lesson => {
+    lessonsObject[lesson]['materia'] === subject ? totalStudents += lessonsObject[lesson]['numeroEstudantes'] : totalStudents;
+  }) 
+  return totalStudents;
+}
+
+function getListOfTeacherClasses(lessonsObject, teacher) {
+  let listOfClasses = [];
+  Object.keys(lessonsObject).forEach(lesson => {
+    lessonsObject[lesson]['professor'] === teacher ? listOfClasses.push(lessonsObject[lesson]['materia']) : listOfClasses;
+  }) 
+  return listOfClasses;
+}
+
+function getNumberOfTeacherStudents(lessonsObject, teacher) {
+  let totalStudents = 0;
+  Object.keys(lessonsObject).forEach(lesson => {
+    lessonsObject[lesson]['professor'] === teacher ? totalStudents += lessonsObject[lesson]['numeroEstudantes'] : totalStudents;
+  }) 
+  return totalStudents;
+}
+
+function createReport(lessonsObject, teacher) {
+  return {
+    professor: teacher,
+    aulas: getListOfTeacherClasses(lessonsObject, teacher),
+    estudantes: getNumberOfTeacherStudents(lessonsObject, teacher)
+  }
+}
+
+console.log(countNumberOfStudentsInClass(allLessons, 'Matemática'));
+console.log(createReport(allLessons, 'Maria Clara'));
