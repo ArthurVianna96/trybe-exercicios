542731c7894e46ceeb6a2281d3cd39384879dd43
Arthur Vianna <arthurpedrosa96.ap@gmail.com>
2022-05-10 16:20:18 -0300

    exercise 2

--- fundamentos/bloco-07/dia-03/functions.test.js
+++ fundamentos/bloco-07/dia-03/functions.test.js
@@ -1,6 +1,6 @@
-const { sum } = require('./functions');
+const { sum, myRemove } = require('./functions');
 
-describe('função sum', () => {
+describe('sum function', () => {
   it('should return 9 if a = 4 and b = 5', () => {
     expect(sum(4, 5)).toBe(9);
   });
@@ -12,5 +12,18 @@ describe('função sum', () => {
   });
   it('should throw an error with the message "parameters must be numbers" when an input is not a number', () => {
     expect(() => sum(4, '5')).toThrow(new Error('parameters must be numbers'));
+  });
+});
+
+describe('myRemove function', () => {
+  const testArray = [1, 2, 3, 4];
+  it('should return an array without the item input if it is contained inside the input array', () => {
+    expect(myRemove(testArray, 3)).toEqual([1, 2, 4]);
+  });
+  it('should not return the same array if the item input is contained inside the input array', () => {
+    expect(myRemove(testArray, 3)).not.toEqual(testArray);
+  });
+  it('should return the same array if the item input is not contained inside the input array', () => {
+    expect(myRemove(testArray, 5)).toEqual(testArray);
   });
 });
